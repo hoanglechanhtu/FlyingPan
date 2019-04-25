778f2b29a266647071ce4a2d9e2a05ab7136df6c
minh.phamquang <phamquangminhex@gmail.com>
2019-04-25 12:35:44 +0700

    - Add function when lava contact with chicken

--- Classes/Lava.h
+++ Classes/Lava.h
@@ -9,9 +9,17 @@ public:
 	Lava(Vec2 position, float scale, int tag, Layer* parent);
 	~Lava();
 
+	void explode(Bullet* bullet);
 	int getEnemyTag();
 
 private:
+	string _currentBullet;
+	Vec2 _fx_Pos;
+	Vec2 _fx_Velocity;
+
 	int _tag = 0;
+
+	bool onContactBegin(PhysicsContact& contact);
+	void createRoastedChicken();
 };
 
