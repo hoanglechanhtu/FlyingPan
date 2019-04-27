cb730a94f77943e55a42b7dd68bcec8d76150321
minh.phamquang <phamquangminhex@gmail.com>
2019-04-27 18:27:38 +0700

    - Add z order as a parameter of Mybutton

--- Classes/MyButton.h
+++ Classes/MyButton.h
@@ -5,6 +5,7 @@ class MyButton : public ui::Button
 {
 public:
 	MyButton(Vec2 position, string path, string path2, Node* parent);
+	MyButton(Vec2 position, string path, string path2, Node* parent, int LocalZOrder);
 	~MyButton();
 };
 
